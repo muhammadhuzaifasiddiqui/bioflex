42ae15b13ea2cd5a3753647f5f90b08723f7ba2c
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-08-19 14:53:55 +0500

    fix build issue

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/layout/Navbar";
 import Hero from "../components/layout/Hero";
-import Service from "../components/layout/Service";
 import Products from "../components/layout/Products";
 import Trusted from "../components/layout/Trusted";
 import Blog from "../components/layout/Blog";
@@ -10,6 +9,7 @@ import Book from "../components/layout/Book";
 import Contact from "../components/layout/Contact";
 import Footer from "../components/layout/Footer";
 import Marquee from "../components/layout/Marquee";
+import Service from "../components/layout/Service";
 
 const page = () => {
   return (
